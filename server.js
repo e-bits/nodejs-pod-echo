6dcbbfac1a7b2778ca6f6915df66389c40047f23
Remo Inderbitzin <remo@inderbitzin.org>
2016-09-25 14:10:35 +0200

    fix: remove process print

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ var os = require("os");
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text'});
 
-  res.end('Welcom on Host/Pod:\n' + os.hostname() + '\n' + process.env.HOSTNAME + '\n');
+  res.end('Welcom on Host/Pod:\n' + os.hostname() + '\n');
 }).listen(8080,'0.0.0.0');
 console.log('Server running.')
