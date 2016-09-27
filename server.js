34917c93d2064f88481703d6a68b0f690d0d334e
Remo Inderbitzin <remo@inderbitzin.org>
2016-09-27 11:19:18 +0200

    feat: print process id instead of hostname

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var http = require('http');
-var os = require("os");
+var process = require('process');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text'});
 
-  res.end('Welcom on Host/Pod:\n' + os.hostname() + '\n');
+  res.end('Welcom on Host/Pod:\n' + process.pid + '\n');
 }).listen(8080,'0.0.0.0');
 console.log('Server running.')
